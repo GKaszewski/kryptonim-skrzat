899331db008a5dd8d3bce795cba976967f95d15a
Gabriel Kaszewski <gabrielkaszewski@gmail.com>
2024-04-25 11:34:40 +0200

    refactor

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,39 +6,30 @@ public class GameManager : MonoBehaviour {
     public CharacterAttributes player;
     public TimeManager timeManager;
     public UIController uiController;
-    
+
     private void Awake() {
         if (Instance == null) {
             Instance = this;
-        } else {
+        }
+        else {
             Destroy(gameObject);
             return;
         }
+
         DontDestroyOnLoad(gameObject);
-        
+
         if (!timeManager) timeManager = GetComponent<TimeManager>();
     }
 
-    private void Start() {
-        GetPlayer();
-    }
-    
-    private void GetPlayer() {
-        if (player != null) return;
-        var playerObject = GameObject.FindWithTag("Player");
-        if (playerObject == null) return;
-        player = playerObject.GetComponent<CharacterAttributes>();
-    }
-    
     public void RestartGame() {
         var currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
         UnityEngine.SceneManagement.SceneManager.LoadScene(currentScene.buildIndex);
     }
-    
+
     public void IncreaseScore(int scoreAmount) {
         player.AddScore(scoreAmount);
     }
-    
+
     public void ResetPlayerScore() {
         player.ResetScore();
     }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [SerializeField] private CharacterAttributes character;
+    
+    private void Start() {
+        if (!character) character = GetComponent<CharacterAttributes>();
+        
+        GameManager.Instance.player = character;
+    }
+}
